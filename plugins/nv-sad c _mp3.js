c9cc093b7752b31ffc078c327ae0b10e1f98ee41
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2024-09-02 16:45:02 +0300

    апяап

--- plugins/nv-sad c _mp3.js
+++ plugins/nv-sad c _mp3.js
@@ -3,11 +3,11 @@ const handler = async (m, {conn, usedPrefix}) => {
   if (!db.data.chats[m.chat].audios) return;
   if (!db.data.settings[conn.user.jid].audios_bot && !m.isGroup) return;
   //const s = seconds: '1934.4'
-  const vn = './media/toma.mp3';
+  const vn = './media/аууу.mp3';
   conn.sendPresenceUpdate('recording', m.chat);
   conn.sendMessage(m.chat, {audio: {url: vn}, ptt: true, mimetype: 'audio/mpeg', fileName: `deja de llorar.mp3`}, {quoted: m});
 };
-handler.command = /^(:c|c)$/i;
+handler.command = /^(:c|ау)$/i;
 handler.fail = null;
 handler.exp = 100;
 export default handler; 

cb8fa7b630d3247efb8c8b3898e6ddcb66b2a4d0
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2024-10-23 17:21:50 +0300

    Update nv-global-1.js

--- plugins/nv-global-1.js
+++ plugins/nv-global-1.js
@@ -290,9 +290,9 @@ this.sendPresenceUpdate('recording', m.chat)
 this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
 
 if (!chat.isBanned && chat.audios && m.text.match(/(лот)/gi)) {    
-let vn = './media/лот.webp'
-this.sendPresenceUpdate('recording', m.chat)   
-this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+    let vn = './media/лот.webp'
+    this.sendPresenceUpdate('recording', m.chat)   
+    this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
 
 
 

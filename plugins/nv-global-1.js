513c5cf92bb52b6cfd3cfe5c6d7189457d6816a9
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2024-10-23 17:11:35 +0300

    екн

--- plugins/nv-global-1.js
+++ plugins/nv-global-1.js
@@ -261,6 +261,48 @@ let vn = './media/доброе утро.webp'
 this.sendPresenceUpdate('recording', m.chat)   
 this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
 
+
+
+
+if (!chat.isBanned && chat.audios && m.text.match(/(пошол нах)/gi)) {    
+let vn = './media/пошол нах.webp'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+    
+if (!chat.isBanned && chat.audios && m.text.match(/(о бот)/gi)) {    
+let vn = './media/о бот.webp'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+    
+if (!chat.isBanned && chat.audios && m.text.match(/(😭)/gi)) {    
+let vn = './media/смайл.webp'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+    
+if (!chat.isBanned && chat.audios && m.text.match(/(вызов)/gi)) {    
+let vn = './media/вызов.webp'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+    
+if (!chat.isBanned && chat.audios && m.text.match(/(доброе)/gi)) {    
+let vn = './media/доброе.webp'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+
+if (!chat.isBanned && chat.audios && m.text.match(/(лот)/gi)) {    
+let vn = './media/лот.webp'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+
+
+
+
+
+
+
+
+
+
    
 return !0 }
 export default handler

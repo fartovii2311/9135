0262208382d23654d6d32906e9911a0f77fa0e01
fartovii2311 <fartovii2311@gmail.com>
2024-11-18 17:52:22 +0300

    Update nv-global-1.js

--- plugins/nv-global-1.js
+++ plugins/nv-global-1.js
@@ -12,11 +12,6 @@ let vn = './media/правила.mp3'
 this.sendPresenceUpdate('recording', m.chat)   
 this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
     
-if (!chat.isBanned && chat.audios && m.text.match(/(ушёл|ушла)/gi)) {    
-let vn = './media/пошёл.mp3'
-this.sendPresenceUpdate('recording', m.chat)   
-this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
-    
 if (!chat.isBanned && chat.audios && m.text.match(/(давай|дайте|дай|Miarda de Bot|Mierda de Bot|Mearda de Bot)/gi)) {    
 let vn = './media/дай.mp3'
 this.sendPresenceUpdate('recording', m.chat)   
